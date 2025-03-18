8c16df1c6f46f8bab2fba88a9d37c6a09910d087
Pavel Koch <kouks.koch@gmail.com>
2025-03-18 23:15:39 +0100

    reverted result

--- sdk/src/test_utils/containers.rs
+++ sdk/src/test_utils/containers.rs
@@ -15,7 +15,7 @@ pub type RedisContainer = ContainerAsync<Redis>;
 
 /// Spins up a Sui container and returns its handle and mapped RPC and faucet
 /// ports.
-pub async fn setup_sui_instance() -> anyhow::Result<(SuiContainer, u16, u16)> {
+pub async fn setup_sui_instance() -> (SuiContainer, u16, u16) {
     let tag = if cfg!(target_arch = "aarch64") {
         "testnet-v1.38.2-arm64"
     } else {
@@ -27,21 +27,39 @@ pub async fn setup_sui_instance() -> anyhow::Result<(SuiContainer, u16, u16)> {
         .with_faucet(true)
         .with_tag(tag);
 
-    let container = sui_request.start().await?;
-    let rpc_port = container.get_host_port_ipv4(9000).await?;
-    let faucet_port = container.get_host_port_ipv4(9123).await?;
+    let container = sui_request
+        .start()
+        .await
+        .expect("Failed to start Sui container.");
 
-    Ok((container, rpc_port, faucet_port))
+    let rpc_port = container
+        .get_host_port_ipv4(9000)
+        .await
+        .expect("Failed to get RPC port.");
+
+    let faucet_port = container
+        .get_host_port_ipv4(9123)
+        .await
+        .expect("Failed to get faucet port.");
+
+    (container, rpc_port, faucet_port)
 }
 
 /// Spins up a Redis container and returns its handle and mapped Redis port.
-pub async fn setup_redis_instance() -> anyhow::Result<(RedisContainer, u16)> {
+pub async fn setup_redis_instance() -> (RedisContainer, u16) {
     let redis_request = Redis::default()
         .with_tag("6.2-alpine")
         .with_env_var("REDIS_PASSWORD", "my_secret_password");
 
-    let container = redis_request.start().await?;
-    let host_port = container.get_host_port_ipv4(6379).await?;
+    let container = redis_request
+        .start()
+        .await
+        .expect("Failed to start Redis container.");
 
-    Ok((container, host_port))
+    let host_port = container
+        .get_host_port_ipv4(6379)
+        .await
+        .expect("Failed to get Redis port.");
+
+    (container, host_port)
 }
--- sdk/tests/object_crawler_test.rs
+++ sdk/tests/object_crawler_test.rs
@@ -74,9 +74,7 @@ enum HeterogeneousValue {
 #[tokio::test]
 async fn test_object_crawler() {
     // Spin up the Sui instance.
-    let (_container, rpc_port, faucet_port) = test_utils::containers::setup_sui_instance()
-        .await
-        .expect("Failed to setup Sui instance.");
+    let (_container, rpc_port, faucet_port) = test_utils::containers::setup_sui_instance().await;
 
     // Build Sui client.
     let sui = sui::ClientBuilder::default()
