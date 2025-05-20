65f6c17ef908e9528badecae423e73fdb1d3d141
afarukcali <afarukcali@gmail.com>
2025-05-20 14:13:03 +0300

    [nexus-tool] fix after merge from main.

--- tools/social-twitter/src/direct_message/mod.rs
+++ tools/social-twitter/src/direct_message/mod.rs
@@ -1,9 +1,8 @@
-pub mod get_conversation_messages_by_id;
-pub mod models;
 //! # `xyz.taluslabs.social.twitter.*`
 //!
 //! This module contains tools for Twitter direct message operations.
 
 pub(crate) mod get_conversation_messages;
+pub(crate) mod get_conversation_messages_by_id;
 pub(crate) mod models;
 pub(crate) mod send_direct_message;
