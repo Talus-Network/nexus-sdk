c93852ae78922ff76ca528f56783a6571b5f6d36
nonast <29281463+nonast@users.noreply.github.com>
2025-10-24 13:12:02 +0800

    feat: add import for sui U256

--- sdk/src/sui.rs
+++ sdk/src/sui.rs
@@ -13,6 +13,7 @@ pub use {
         ident_str as move_ident_str,
         identifier::IdentStr as MoveIdentStr,
         language_storage::{StructTag as MoveStructTag, TypeTag as MoveTypeTag},
+        u256::U256 as MoveU256,
     },
     shared_crypto::intent::Intent,
     sui_config::{
