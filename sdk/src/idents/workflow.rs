d2d598d4f1c60e760a34a9a5295cd2ad4bcae531
nonast <29281463+nonast@users.noreply.github.com>
2026-01-22 16:27:50 +0800

    chore: fmt

--- sdk/src/idents/workflow.rs
+++ sdk/src/idents/workflow.rs
@@ -405,6 +405,13 @@ impl Dag {
         module: DAG_MODULE,
         name: sui::types::Identifier::from_static("submit_on_chain_tool_eval_for_walk"),
     };
+    /// Convert TaggedOutput to DAG types.
+    ///
+    /// `nexus_workflow::dag::tagged_output_to_dag_types`
+    pub const TOOL_OUTPUT_TO_DAG_TYPES: ModuleAndNameIdent = ModuleAndNameIdent {
+        module: DAG_MODULE,
+        name: sui::types::Identifier::from_static("tagged_output_to_dag_types"),
+    };
     /// The Vertex struct. Mostly used for creating generic types.
     ///
     /// `nexus_workflow::dag::Vertex`
@@ -728,14 +735,6 @@ impl Dag {
             }
         }
     }
-
-    /// Convert TaggedOutput to DAG types.
-    ///
-    /// `nexus_workflow::dag::tagged_output_to_dag_types`
-    pub const TOOL_OUTPUT_TO_DAG_TYPES: ModuleAndNameIdent = ModuleAndNameIdent {
-        module: DAG_MODULE,
-        name: sui::types::Identifier::from_static("tagged_output_to_dag_types"),
-    };
 }
 
 // == `nexus_workflow::tool_registry` ==
